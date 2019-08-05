b44badf03fa085784431001ea08fb0137df44601
VisalYun <47347693+VisalYun@users.noreply.github.com>
2019-08-05 20:45:24 +0700

    Updated

--- MP3_Player/app/src/main/java/com/learnprogramming/mp3_player/MainActivity.kt
+++ MP3_Player/app/src/main/java/com/learnprogramming/mp3_player/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.learnprogramming.mp3_player
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,7 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import android.widget.Toast
 import android.widget.ViewAnimator
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,7 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mp: MediaPlayer
     var position =0
-    var index:Int = 10
+    var index:Int = 0
+    var start:Boolean = true
+    var vol:Int =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,38 +30,56 @@ class MainActivity : AppCompatActivity() {
         song_list.adapter = arrayAdapter
 
         song_list.setOnItemClickListener{ adapterView, view, i, l ->
-//            if(mp.isPlaying()){
-//                mp.pause()
-//                index = i
-//                play(index)
-//                Toast.makeText(this,"${songArrays[i]} is playing...",Toast.LENGTH_SHORT).show()
-//            }
-//            else{
-                index = i
+            index = i
+            if(start == true){
+                start=false
                 play(index)
                 Toast.makeText(this,"${songArrays[i]} is playing...",Toast.LENGTH_SHORT).show()
-            //}
+            }
+            else{
+                mp.pause()
+                play(index)
+                Toast.makeText(this,"${songArrays[i]} is playing...",Toast.LENGTH_SHORT).show()
+                pause.isEnabled = true
+            }
         }
+
+        val am = getSystemService(AUDIO_SERVICE) as AudioManager
+        volume.max = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        volume.progress = am.getStreamVolume(AudioManager.STREAM_MUSIC)
+        volume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                am.setStreamVolume(AudioManager.STREAM_MUSIC, p1, AudioManager.FLAG_SHOW_UI)
+                vol = p1
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 
     fun oca(){
         mp = MediaPlayer.create(this, R.raw.one_call_away)
+        album.setImageResource(R.drawable.ntm)
         mp.start()
     }
     fun ct(){
         mp = MediaPlayer.create(this, R.raw.cheap_thrill)
+        album.setImageResource(R.drawable.tia)
         mp.start()
     }
     fun dwk(){
         mp = MediaPlayer.create(this, R.raw.dwk)
+        album.setImageResource(R.drawable.v)
         mp.start()
     }
     fun ll(){
         mp = MediaPlayer.create(this, R.raw.lily)
+        album.setImageResource(R.drawable.dw)
         mp.start()
     }
     fun b(){
         mp = MediaPlayer.create(this, R.raw.boy)
+        album.setImageResource(R.drawable.vn)
         mp.start()
     }
 
@@ -92,8 +115,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun con(v:View){
-        if(index == 10){
+        if(start == true){
             oca()
+            play.isEnabled=false
+            start = false
         }
         else{
             if (mp.isPlaying() == false) {
